689471a64e2bd942493af87d91e3fbfc3c0fb302
artemegerev <artemegerev@MacBook-Air-Artem.local>
2017-05-17 18:52:40 +0300

    hui

--- MusicPlayer_bot.py
+++ MusicPlayer_bot.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot('278758102:AAENnsnUdlaLTklqCaoj0F8hSckxoR-_t10')
 
+
 @bot.message_handler(commands=['start'])
 def default_test(message):
     bot.send_message(message.from_user.id, 'Please input audio track title or performer\'s name.')
@@ -12,26 +13,27 @@ def default_test(message):
 
 @bot.message_handler(content_types=["text"])
 def _query(message):
-    url = 'https://downloadmusicvk.ru/audio/search?q=' + parser(message.text)
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text, "html.parser")
-    counter = 1
-    for link in soup.findAll('div', {'class' : 'row audio'}, limit=5):
-        name_of_song = link.find('div', {'class' : 'col-lg-9 col-md-8 col-sm-7 col-xs-5'}).text
-        # go to 3rd block
-        link_to_download = link.find('div', {'class' : 'col-lg-2 col-md-3 col-sm-4 col-xs-5'})
-        # go to download button
-        link_to_download = link_to_download.find('a', {'class' : 'btn btn-primary btn-xs download'})
-
-        # link to page to download
-        main_link = 'https://downloadmusicvk.ru' + link_to_download.get('href')
-        # link to download
-        answer = handle_song(main_link)
-
-        code = '<a href=\"' + answer + '\">' + str(counter) + '. ' + name_of_song.strip() + '</a>'
-        bot.send_message(message.from_user.id, parse_mode='HTML', text = code)
-        counter += 1
+    # url = 'https://downloadmusicvk.ru/audio/search?q=' + parser(message.text)
+    # source_code = requests.get(url)
+    # plain_text = source_code.text
+    # soup = BeautifulSoup(plain_text, "html.parser")
+    # counter = 1
+    # for link in soup.findAll('div', {'class' : 'row audio'}, limit=5):
+    #     name_of_song = link.find('div', {'class' : 'col-lg-9 col-md-8 col-sm-7 col-xs-5'}).text
+    #     # go to 3rd block
+    #     link_to_download = link.find('div', {'class' : 'col-lg-2 col-md-3 col-sm-4 col-xs-5'})
+    #     # go to download button
+    #     link_to_download = link_to_download.find('a', {'class' : 'btn btn-primary btn-xs download'})
+    #
+    #     # link to page to download
+    #     main_link = 'https://downloadmusicvk.ru' + link_to_download.get('href')
+    #     # link to download
+    #     answer = handle_song(main_link)
+    #
+    #     code = '<a href=\"' + answer + '\">' + str(counter) + '. ' + name_of_song.strip() + '</a>'
+    #     bot.send_message(message.from_user.id, parse_mode='HTML', text = code)
+    #     counter += 1
+    bot.send_audio(message.from_user.id, caption="Егор Крид", audio="http://mjcdn.cc/2/310716274/0JXQs9C+0YAg0JrRgNC40LQgJiBNT0xMWSAtINCV0YHQu9C4INGC0Ysg0LzQtdC90Y8g0L3QtSDQu9GO0LHQuNGI0YwubXAz/")
 
 def handle_song(main_link):
     source_code = requests.get(main_link)
